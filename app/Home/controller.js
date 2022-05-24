948493e12b7d94fb45d2cd6b4e5382fb4281ed3f
Angry Coding <mailidanielifechukwu@gmail.com>
2022-05-24 13:03:49 +0100

    correct numbers

--- app/Home/controller.js
+++ app/Home/controller.js
@@ -1,5 +1,25 @@
-const index = function (req, res) {
-  res.send("SUCCESS");
+import Student from "../Students/model";
+import Staff from "../Staff/model";
+
+// const index = function (req, res) {
+//   res.send("SUCCESS");
+// };
+
+const index = async (req, res) => {
+  let staffs = await Staff.find({});
+  for (const staff of staffs) {
+    let newNumber = staff.regNumber.replaceAll("/", "-");
+    await Staff.update({ _id: staff._id }, { admissionNumber: newNumber });
+  }
+  let students = await Student.find({});
+  for (const student of students) {
+    let newAdmissionNumber = student.admissionNumber.replaceAll("/", "-");
+    await Student.update(
+      { _id: student._id },
+      { admissionNumber: newAdmissionNumber }
+    );
+  }
+  res.send("lala");
 };
 module.exports = {
   index,
